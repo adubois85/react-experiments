704cf6c741ee6d0db3cb3db90b5da49b28a19591
Alex DuBois <adubois@alumni.uci.edu>
2022-07-28 14:46:49 -0600

    shelving things for now, will pick up later

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
-import Game from './TicTacToe/Game'
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import App from './App';
+import Game from './components/TicTacToe/Game';
 
-ReactDOM.render(
-    <React.StrictMode>
-        {/* <App /> */}
-        <Game />
-    </React.StrictMode>,
+const root = ReactDOM.createRoot(
     document.getElementById('root')
 );
+root.render(
+    <Router>
+        <Routes>
+            <Route path="/" element={<App />}>
+                 <Route path="/tictactoe" element={<Game />} />
+            </Route>
+        </Routes>
+    </Router>
+);
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//         {/* <Game /> */}
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
