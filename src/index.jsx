58aac502c03cf3a2c659253b43b3cfcae0f9cbe9
Alex DuBois <adubois@alumni.uci.edu>
2023-07-01 17:55:41 -0600

    cleaning up unused template stuff

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ root.render(
     <Router>
         <Routes>
             <Route path="/" element={<App />}>
-                 <Route path="/tictactoe" element={<Game />} />
+                <Route path="/tictactoe" element={<Game />} />
             </Route>
         </Routes>
     </Router>
