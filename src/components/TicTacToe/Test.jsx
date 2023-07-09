8a65b7f37ed06d4ea44ef2abf938a104f78ad7c0
Alex DuBois <adubois@alumni.uci.edu>
2023-07-08 19:09:13 -0600

    something different; trying to use a bitboard

--- src/components/TicTacToe/Test.jsx
+++ src/components/TicTacToe/Test.jsx
@@ -3,106 +3,81 @@ import Board from './Board'
 import '../../stylesheets/TicTacToe.css'
 
 function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ]
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i]
-        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a]
+    /* 
+    a bitboard representation of the winning moves
+    ┌────────────────┐
+    │  1 │   2 │   4 │ 
+    ├────┼─────┼─────┤
+    │  8 │  16 │  32 │
+    ├────┼─────┼─────┤
+    │ 64 │ 128 │ 256 │
+    └────────────────┘
+    add up each row/column/diagonal to get a sum of winning squares
+    */
+    const wins = [7, 56, 448, 73, 146, 292, 84, 273]
+    wins.forEach(win => {
+        // use each winning sum as a bitmask, then see if any of the results are equal;
+        // if so, then you must have filled in those squares, thus winning
+        let temp = squares & win
+        if (temp == win){
+            console.log("You win")
         }
-    }
+    })
 }
 
 export default function Test() {
-    const [game, setGame] = useState({
-        history: [Array(9).fill(null),
-],
-        stepNumber: 0,
-        xIsNext: true,
-        winner: null
-    })
+    // console.log(calculateWinner(8))
+    // let temp = [7, 56, 448, 73, 146, 292, 84, 273]
+    // let score = 401
+    // console.log([score | 7, score | 56, score | 448],
+    //             [score | 73, score | 146, score | 292],
+    //             [score | 84, score | 273])
 
-    function handleClick(i){
-        let currentMove = game.history.slice(-1)[0]
-        if (currentMove[i] || game.winner) {
-            return
-        }
-        currentMove[i] = game.xIsNext ? 'X' : 'O'
-        // console.log(currentMove)
-        setGame(prevGame => ({
-            history: [...prevGame.history, currentMove],
-            stepNumber: prevGame.stepNumber + 1,
-            xIsNext: !prevGame.xIsNext,
-            winner: calculateWinner(currentMove)
-        }))
-        // console.log(game.history)
-    }
+    // console.log([score & 7, score & 56, score & 448],
+    //             [score & 73, score & 146, score & 292],
+    //             [score & 84, score & 273])
 
-    const gameStatus = (game.winner)
-                        ? `Winner: ${game.winner}`
-                        : `Next Player: ${game.xIsNext ? 'X' : 'O'}`
+    // console.log([score ^ 7, score ^ 56, score ^ 448],
+    //             [score ^ 73, score ^ 146, score ^ 292],
+    //             [score ^ 84, score ^ 273])
 
-    // const moves = game.history.map(move => {
-    //     const desc = (move)
-    //                 ? `Go to move # ${move}`
-    //                 : "Go to game start"
-    //     return(
-    //         <li key={move}>
-    //             <button onClick={() => {
-    //                 setGame(prevGame => ({
-    //                     ...prevGame,
-    //                     history: prevGame.history.slice(0, move)
-    //                 }))
-    //             }}>
-    //                 {desc}
-    //             </button>
-    //         </li>
-    //     )
-    // })
-    function jumpTo(step) {
-        setGame(prevGame => ({
-            ...prevGame,
-            history: prevGame.history.slice(0, step + 1),
-            stepNumber: step,
-            xIsNext: step % 2,
-        }))
-    }
+    // console.log(calculateWinner(146))
+    calculateWinner(137)
 
-    const moves = game.history.map((move, index) => {
-            let description = (index == 0)
-                                ? 'game start'
-                                : `move #${index}`
-            return (
-                <li key={index}>
-                    <button onClick={() => jumpTo(index)}>
-                        Go to {description}
-                    </button>
-                </li>
-            );
-        });
+    const dummyBoard = Array(9).fill(null)
+    // const gameStatus = (game.winner)
+    //                     ? `Winner: ${game.winner}`
+    //                     : `Next Player: ${game.xIsNext ? 'X' : 'O'}`
+
+    // const moves = game.history.map((move, index) => {
+    //         let description = (index == 0)
+    //                             ? 'game start'
+    //                             : `move #${index}`
+    //         return (
+    //             <li key={index}>
+    //                 <button onClick={() => jumpTo(index)}>
+    //                     Go to {description}
+    //                 </button>
+    //             </li>
+    //         );
+    //     });
+
+{/* <div className="game-info">
+    <div>{gameStatus}</div>
+</div> */}
+//     <ol>
+//     {moves}
+// </ol>
     
     return (
         <div className="game">
             <div className="game-board">
                 <Board
-                    squares={game.history[0]}
+                    squares={dummyBoard}
                     onClick={(i) => handleClick(i)}
                 />
             </div>
-            <div className="game-info">
-                <div>{gameStatus}</div>
-            </div>
-            <ol>
-                {moves}
-            </ol>
+
         </div>
     )
 }
